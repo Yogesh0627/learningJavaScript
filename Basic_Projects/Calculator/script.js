b3ce55e88f05c838e30b37d1607491eb476c5f89
Yogesh <chauhanyogesh950@gmail.com>
2023-12-26 19:50:56 +0530

    Some Basic Project in JavaScript

---
+++ Basic_Projects/Calculator/script.js
@@ -0,0 +1,50 @@
+// const array = document.querySelectorAll('button')
+// console.log(array)
+// let string=''
+
+// array.forEach((element)=> element.addEventListener('click',(e)=>{
+
+//     let targetNumber = e.target.innerText;
+//     console.log(targetNumber)
+//     if (targetNumber === 'c'){
+//         string = '';
+//         document.getElementById('screen').value=''
+//     }
+//     else if (targetNumber === '='){
+//         document.getElementById('screen').value = eval(string)
+//         console.log(string)
+//     }
+//     else{
+//         string += targetNumber
+//         document.getElementById('screen').value = string
+//     }
+// }))
+
+// let array = [1,2,3,4,5,6,7,8]
+//  const newAray=array.map((item)=>{})
+
+
+// let buttons = document.querySelectorAll('.btn')
+// let string =''
+// let display = document.getElementById('screen')
+// // console.log(display)
+// buttons.forEach((element)=> element.addEventListener('click',(e)=>{
+//     console.log(e.target.innerText)
+//     let targetNumber = e.target.innerText
+    
+//     if ( targetNumber === 'c'){
+//         display.value=''
+//     }
+
+//     else if (targetNumber === '='){
+//         display.value = eval(string)
+//     }
+
+//     else{
+//         string += targetNumber
+//         display.value = string
+//     }
+
+// }))
+
+
